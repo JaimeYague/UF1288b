7f59057f5a2605a156881c1d37fa13fee4af3185
unknown <jaimeyaguem@gmail.com>
2025-05-09 08:46:43 +0200

    ruta clienttwo fix

--- server.py
+++ server.py
@@ -10,6 +10,11 @@ def confirmacion():
 def clientone():
     return "mensaje de cliente uno"
 
-@app.route("/clientwo")
+@app.route("/clienttwo")
 def clienttwo():
     return "mensaje de cliente dos"
+
+
+if __name__ == '__main__':
+    # Si se ejecuta server.py, entonces inicia el servidor Flask
+    app.run(host='0.0.0.0', port=5000)
